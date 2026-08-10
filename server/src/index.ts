6972e5f5ca1925431bb629a94ee1f1afc0b6b8f2
swamiabhishek45 <abhishekswami1435@gmail.com>
2026-08-10 16:31:23 +0530

    add workspace routes, controllers, services and business logic

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 import { toNodeHandler } from "better-auth/node";
 import { auth } from "./lib/auth.js";
+import { registerRoutes } from "./routes/index.js";
+import { errorHandler } from "./middleware/error-handler.middleware.js";
 
 
+const clientUrl = process.env.CLIENT_URL ?? "http://localhost:3001";
 const app = express();
 const PORT = process.env.PORT || 8081;
+
+app.use(
+    cors({
+        origin: clientUrl,
+        credentials: true,
+    }),
+);
 
 app.all('/api/auth/{*any}', toNodeHandler(auth));
 
@@ -18,6 +29,10 @@ app.get("/", (_req, res) => {
 app.get("/health", (_req, res) => {
     res.json({ status: "ok" });
 });
+
+registerRoutes(app);
+
+app.use(errorHandler)
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
---
+++ server/src/routes/index.ts
@@ -0,0 +1,7 @@
+import type { Express } from "express";
+import { workspaceRoutes } from "./workspace.routes.js";
+
+
+export function registerRoutes(app:Express):void{
+    app.use("/api/workspaces" , workspaceRoutes)
+}

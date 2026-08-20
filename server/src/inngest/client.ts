d9c8eb62af912a5e2082c07694633adbc573cadc
swamiabhishek45 <abhishekswami1435@gmail.com>
2026-08-20 14:19:46 +0530

    feat: initialize Inngest client and define source/created event schema

--- server/src/inngest/client.ts
+++ server/src/inngest/client.ts
@@ -1,14 +1,7 @@
 import { Inngest } from "inngest";
-import "dotenv/config";
-
-const isDev = process.env.NODE_ENV !== "production" && process.env.INNGEST_DEV === "1";
 
 // Create a client to send and receive events
-export const inngest = new Inngest({
-    id: "chaibooklm",
-    eventKey: process.env.INNGEST_EVENT_KEY,
-    isDev,
-});
+export const inngest = new Inngest({ id: "chaibooklm" });
 
 
 
